e1b8e39eea1acd296484184dc9a4d6cc9b6c8e16
Nicholas Joseph <nicholasjoseph1994@gmail.com>
2015-04-26 12:44:48 -0400

    add new officers emails

--- handlers/Email.py
+++ handlers/Email.py
@@ -9,7 +9,7 @@ class Email(Handler):
 		overdueGear = [g for g in Gear.all() if g.returnDate and datetime.now() > g.returnDate]
 		for g in overdueGear:
 			sender_address = 'overduegear@hmc-gear.appspotmail.com'
-			destination_address = [g.holder.email, 'njoseph@college.harvard.edu', 'wbloxham@college.harvard.edu', 'rradovanovic@college.harvard.edu']
+			destination_address = [g.holder.email, 'rorybrown@college.harvard.edu', 'danielbridgwater@college.harvard.edu', 'njoseph@college.harvard.edu', 'wbloxham@college.harvard.edu', 'rradovanovic@college.harvard.edu']
 			subject = 'Overdue Gear'
 			body = "You have a piece of gear that is overdue, " + g.number + ". Please return it at the next meeting. If you can't make it please email one of the gear czars to find a time."""
 			mail.send_mail(sender_address, destination_address, subject, body)
